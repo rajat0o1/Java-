7bcf743cbf10e935b7c352e57238d32a5980e2f2
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-12 16:51:48 +0000

    Configuration DOne

---
+++ Arrays/Arr2.java
@@ -0,0 +1,8 @@
+class Test{
+    public static void main(String[] args){
+        int a[]=new int [5];
+         for (int j =0; j<a.length;j++){
+            System.out.println(j);
+         }
+    }
+}
