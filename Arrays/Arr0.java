3746b3ff2227b79dd583638127795cd0c24e67e9
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-12 16:39:02 +0000

    Configuration Done

---
+++ Arrays/Arr0.java
@@ -0,0 +1,7 @@
+
+class sample Array{
+    public static void main(String[] args){
+        int []a={12,20,30,40};
+        System.out.println(a);
+    }
+}
--- Arrays/Arr1.java
+++ Arrays/Arr1.java
@@ -1,7 +1,29 @@
 class Arrayy{
     public  static void main(String[] args){
+
+
+        // Static Declaration of Arrays-->
         int arr[]={1,2,3};
+                System.out.println(arr.hashCode());
+
         System.out.println(arr[0]);
+
+                System.out.println(arr[1]);
+        System.out.println(arr[02]);
+
+
+
+        // Dynamic Declaration of Arrays-->
+        int arrr[]=new int[3];
+        arrr[0]=200;
+        arrr[1]=400;
+        arrr[2]=600;
+        System.out.println(arrr[0]);
+        System.out.println(arrr[1]);
+        System.out.println(arrr[2]);
+
+
+    
     
     } 
 }
