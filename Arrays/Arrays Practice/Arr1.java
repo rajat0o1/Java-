673cba2eca12a229dadb45677c3eab82f54251f2
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-13 17:19:20 +0000

    Configuration done

---
+++ Arrays/Arrays Practice/Arr1.java
@@ -0,0 +1,13 @@
+//  WAP to create an int array with size 10.
+class Test{
+    public static void main(String[] args){
+        int a[]=new int[10];
+        for (int i=0;i<a.length;i++){
+            a[i]=i;
+
+        }
+        for (int j=0;i<a.length;j++){
+            System.out.println(a[j]);
+        }
+    }
+}
