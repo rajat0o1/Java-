ba2222398ac68f501bc9f886f5015d80e4310582
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-14 18:33:48 +0000

    Configuration Done

--- Arrays/Arrays Practice/Arr4.java
+++ Arrays/Arrays Practice/Arr4.java
@@ -1,12 +1,9 @@
-// * WAP to create an array with size 10. and initialize all element with 0.
-
 class Test{
     public static void main(String[] args){
         int arr[]=new int[10];
         for(int i=0;i<arr.length;i++){
-            arr[i]=0;
+            arr[i]=i+1;
             System.out.println(arr[i]);
         }
-
     }
 }
