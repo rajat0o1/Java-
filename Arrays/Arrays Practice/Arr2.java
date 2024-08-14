931b7d0767b476993f09c0080cb2a5dbf039218a
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-14 18:26:27 +0000

    Configuration Done

--- Arrays/Arrays Practice/Arr2.java
+++ Arrays/Arrays Practice/Arr2.java
@@ -1,9 +1,8 @@
+//    WAP to create a float array with size 100.
+
 class Test{
     public static void main(String[] args){
         float array[]=new float[20];
-        System.out.println(array[0]);
-        System.out.println(array[1]);
-        System.out.println(array[2]+"\n");
         for (int  i=0;i<=10;i++){
               array[i] = i ;
             System.out.println(array[i]);
---
+++ Arrays/Arrays Practice/Arr3.java
@@ -0,0 +1,8 @@
+// * WAP to create an array with size 10. and initialize all element with 0.
+
+class Test{
+    public static void main(String[] args){
+        int arr[]=new int[10];
+        
+    }
+}
