1c0bc11a7a5a26bb8a8aafcbe46683ac337532c8
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-15 05:31:19 +0000

    Configuration Done

--- Arrays/Arrays Practice/Arr5.java
+++ Arrays/Arrays Practice/Arr5.java
@@ -1,1 +1,12 @@
 // * WAP to create an array with size 10 and assign a value using keyboard and after reading display the value of array.
+
+import java.util.Scanner;
+class Test{
+    public static void main(String[] args){
+        int SIZE=15;
+        int []arr=new int[SIZE];
+        Scanner sc=new Scanner(System.in);
+
+
+    }
+}
