a0856949de5544229731d976a276042bf1db6f09
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-16 04:08:11 +0000

    Configuration Done

--- Arrays/Arrays Practice/Arr10.java
+++ Arrays/Arrays Practice/Arr10.java
@@ -1,7 +1,7 @@
 // * WAP to create an array with size 10 and initialize the array element
 //  using keyboard and display the square of each element
 import java.util.Scanner;
-class Test{
+class ArrayElemSquare{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int arr[]=new int[10];
@@ -9,8 +9,9 @@ class Test{
             System.out.print("Please Enter Array Element "+(i+1)+": ");
             
             arr[i]=sc.nextInt();
-            
+
                 }
+            System.out.println();
         for (int i=0;i<arr.length;i++){
             // arr[i]=(i+1);
                     int square =arr[i]*arr[i];
