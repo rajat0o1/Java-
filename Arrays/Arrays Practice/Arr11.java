05ad29922f674c11afd20f4095b10585ed7d4bdb
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-16 13:09:39 +0000

    Configuration Done

--- Arrays/Arrays Practice/Arr11.java
+++ Arrays/Arrays Practice/Arr11.java
@@ -1,20 +1,23 @@
 // * WAP to create an array with size 10 and initialize the array element using keyboard and
 //  display the smallest and largest elements
 
-class Test{
-    public static void main(String[] args){
-        int arr[]=new int[10];
-        for(int i=0;i<arr.length;i++){
-            
-            arr[i]=(i+1);
+ class Test {
+    public static void main(String[] args) {
+        int[] arr = {50, 45, 45, 60, 4558, 455, 789, 8844,  46};
+        
+        int Largest = arr[0];
+        int Smallest = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < Smallest) {
+                Smallest = arr[i];
+            }
+            if (arr[i] > Largest) {
+                Largest = arr[i];
+            }
         }
 
-
-        
-        for(int i=0;i<arr.length;i++)
-
-        System.out.println(arr[i]);
-        
-        
+        System.out.println("Smallest number: " + Smallest);
+        System.out.println("Largest number: " + Largest);
     }
 }
