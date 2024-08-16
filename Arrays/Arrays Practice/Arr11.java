854de6a2fd59a922e1a4d9cf481174405a63d309
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-16 12:41:01 +0000

    Configuration Done

---
+++ Arrays/Arrays Practice/Arr11.java
@@ -0,0 +1,20 @@
+// * WAP to create an array with size 10 and initialize the array element using keyboard and
+//  display the smallest and largest elements
+
+class Test{
+    public static void main(String[] args){
+        int arr[]=new int[10];
+        for(int i=0;i<arr.length;i++){
+            
+            arr[i]=(i+1);
+        }
+
+
+        
+        for(int i=0;i<arr.length;i++)
+
+        System.out.println(arr[i]);
+        
+        
+    }
+}
