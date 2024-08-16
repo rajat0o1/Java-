8078fb69e8a2246f67191a54326d56e51a7d848e
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-16 13:12:19 +0000

    Configuration Done

--- Arrays/Arrays Practice/Arr3.java
+++ Arrays/Arrays Practice/Arr3.java
@@ -4,7 +4,7 @@ class Test{
     public static void main(String[] args){
         int arr[]=new int[10];
         for(int i=0;i<arr.length;i++){
-            arr[i]=0;
+            arr[i]=i+1;
             System.out.println(arr[i]);
             
         }
