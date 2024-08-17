3166b80170ebf461f2b1d7d6b9378a88ccf5f87f
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-17 14:19:54 +0000

    Configuration Done

---
+++ Arrays/Arrays Practice/Arr13.java
@@ -0,0 +1,22 @@
+//* search the given element into an array.
+import java.util.Scanner;
+class Test{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please Enter the Number: ");
+        int Given=0;
+        Given=sc.nextInt();
+        int arr[]= {1,2,3,4,5,6,7};
+        for(int i=0;i<arr.length;i++){
+            if (arr[i]==Given);{
+                
+            System.out.println("Matched "+arr[i]+" is at index "+i);
+            }
+
+        }
+        
+          
+
+
+    }
+}
