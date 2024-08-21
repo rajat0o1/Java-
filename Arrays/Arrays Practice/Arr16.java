3b27728f5d7aec3a6ae61226d4543324819e3ea7
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-21 04:29:00 +0000

    Configuration Done

--- Arrays/Arrays Practice/Arr16.java
+++ Arrays/Arrays Practice/Arr16.java
@@ -3,6 +3,8 @@ class Test{
     public static void main(String[] args){
         int EvenCount=0,OddCount=0;
         int []arr={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+
+
         for(int i=0;i<arr.length;i++){
             if (arr[i]%2==0){
              EvenCount++;
