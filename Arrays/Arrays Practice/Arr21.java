9ec4f250cd2e565883ff28ac773d77dbbd678b1e
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-24 15:01:20 +0000

    Configuration Done

--- Arrays/Arrays Practice/Arr21.java
+++ Arrays/Arrays Practice/Arr21.java
@@ -2,21 +2,16 @@
 import java.util.Scanner;
  class Main {
     public static void main(String[] args) {
-        int[] originalArray = {1, 2, 3}; // Original array
-        int elementToAdd = 4; // Element to add
-        
-        // Create a new array with one more slot
+        int[] originalArray = {1, 2, 3}; 
+        int elementToAdd = 4; 
         int[] newArray = new int[originalArray.length + 1];
         
-        // Copy elements from the original array to the new array
         for (int i = 0; i < originalArray.length; i++) {
             newArray[i] = originalArray[i];
         }
         
-        // Add the new element to the end of the new array
         newArray[originalArray.length] = elementToAdd;
         
-        // Print the new array
         for (int value : newArray) {
             System.out.print(value + " ");
         }
