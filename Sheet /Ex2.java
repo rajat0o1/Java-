6e211fc3bb0f7b2c35cf8df30c9b7464973490d2
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-24 15:00:09 +0000

    Configuration Done

--- Sheet /Ex2.java
+++ Sheet /Ex2.java
@@ -4,7 +4,7 @@ class MetersToCentimeters{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int x=0;
-        System.out.println("Enter numbers in Meters: ");
+        System.out.print("Enter numbers in Meters: ");
         x=sc.nextInt();
         int y=x*100;
         System.out.println("Your Number in Centimeters is: "+y);
