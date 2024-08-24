6a4393639abb651beab7406b09a8c9588794b283
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-24 14:44:26 +0000

    ChangesCommited

---
+++ Sheet /Ex2.java
@@ -0,0 +1,15 @@
+//Take a number a meters, convert it to centimeters, then print it on the screen
+import java.util.Scanner;
+class MetersToCentimeters{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=0;
+        System.out.println("Enter numbers in Meters: ");
+        x=sc.nextInt();
+        int y=x*100;
+        System.out.println("Your Number in Centimeters is: "+y);
+
+
+
+    }
+}
