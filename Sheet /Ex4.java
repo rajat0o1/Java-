1e605214289875acfd37358d571069c91889e09f
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-24 15:09:59 +0000

    Configuration Done

--- Sheet /Ex4.java
+++ Sheet /Ex4.java
@@ -1,11 +1,13 @@
 // Take number of inches , convert it to feet, then print it on the screen
 import java.util.Scanner;
-class InchestoFeet{
+class InchesToFeet{
     public static void main(String[] args){
         int x=0;
-        System.out.print("Enter number in Inches: ")
+        System.out.print("Enter number in Inches: ");
         Scanner sc=new Scanner(System.in);
         x=sc.nextInt();
+        int y=x*12;
+        System.out.println("Your answer is: "+y);
 
     }
 }
