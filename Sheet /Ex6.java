557a3967584d5b3563d0c0f671d28fa162433552
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-25 04:16:40 +0000

    Configuration Done

--- Sheet /Ex6.java
+++ Sheet /Ex6.java
@@ -2,14 +2,14 @@
 
 import java.util.Scanner;
 
-class Test{
+class Square{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter Side of Square: ");
 
         int side=sc.nextInt();
-        int perimeter=4*side;
         side=side*side;
+        int perimeter=4*side;
 
         int area=side;
         System.out.println("Perimeter: "+perimeter);
