146734c934813b3a9f57756289044a41b1df5bd7
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-25 05:02:13 +0000

    Configuration Done

---
+++ Sheet /Ex11.java
@@ -0,0 +1,24 @@
+//  Take a number (say 'n') , calculate the 'nth' even numbers, print it on the screen 
+
+import java.util.Scanner;
+class NThEven{
+    public static void main(String[] args){
+        int n=0;
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Number: ");
+        
+        n=sc.nextInt();
+
+        int even=0;
+
+        for(int i=0;i<=n*2;i++){
+
+            if (i%2==0){
+                even=(n*2);
+
+            }
+        }
+        System.out.println(even);
+    }
+}
