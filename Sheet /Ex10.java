0e29eda6b2f56fa50f66e8c4bf2341b4f4d0fd93
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-08-26 13:12:28 +0000

    Configuration Done

--- Sheet /Ex10.java
+++ Sheet /Ex10.java
@@ -1,17 +1,24 @@
-//  Take a number (say 'n') , calculate sum of first 'n' even numbers, print it on the screen 4 20
+// Take a number (say 'n') , calculate sum of first 'n' even numbers, print it on the screen
+
 
 import java.util.Scanner;
-class NEven{
+
+class SumOfFirstNEven{
     public static void main(String[] args){
-        int n=0;
-        Scanner sc= new Scanner(System.in);
-        System.out.print("Enter Number: ");
+        Scanner sc=new Scanner(System.in);
+        int sum=0,n=0;
+        System.out.println("Enter Number: ");
         n=sc.nextInt();
         for (int i=0;i<=n*2;i++){
-            if (i%2==0){
-                System.out.println(i);
+            if (i%2 == 0){
+                sum= sum+i;
+
             }
+
         }
+        
+        System.out.println("Sum of First "+ n + " Even number is: "+sum);
+
 
     }
 }
