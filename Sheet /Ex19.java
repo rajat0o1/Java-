e77568dfebc1025081b455da9a6872886d80451e
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-09-01 17:54:12 +0000

    Configuration Done

--- Sheet /Ex19.java
+++ Sheet /Ex19.java
@@ -8,11 +8,14 @@ class ThirdAngle{
         int x=sc.nextInt();
         System.out.println("Enter 2nd side of the triangle");
         int y=sc.nextInt();
-              if (x <= 0 || y <= 0 || x+y >= 180) {
-                System.out.println("Invalid angles. The sum of the two angles must be less than 180 degrees and both angles must be positive.");
+        if (x <= 0 || y <= 0 || x+y >= 180) {
+        System.out.println("Invalid angles. The sum of the two angles must be less than 180 degrees and both angles must be positive.");
+        }
         int z=x+y;
+        int l=z-180;
         
-        System.out.println("The 3rd angle of the trianle is"+180-z);
+        
+        System.out.println("The 3rd angle of the trianle is: "+l);
 
 
     }
