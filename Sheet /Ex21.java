793101c0bb37eb1850938c91e2c372f5cf04b3a2
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-09-01 22:27:47 +0000

    Configuration Done

---
+++ Sheet /Ex21.java
@@ -0,0 +1,18 @@
+// 21 Write a program to calculate area of an equilateral triangle
+
+import java.util.Scanner;
+class EquilateralTriangleArea{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter side of the trianlge");
+        double side=sc.nextDouble();
+
+        double area= ( 1.73 * side * side )/4;  
+        System.out.println("Area  of Equilateral Triangle is: "+ area);
+
+
+        
+
+
+    }
+}
