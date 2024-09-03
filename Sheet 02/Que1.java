7da7e6a3d853f00ab33a416fd17b89545a3a8505
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-09-03 05:18:46 +0000

    Configuration Done

---
+++ Sheet 02/Que1.java
@@ -0,0 +1,9 @@
+// 1 Take 2 numbers as input from user, print the largest number on the screen
+
+
+import java.util.Scanner;
+class LargestOfTwo{
+    public static void main(String[] args){
+        
+    }
+}
