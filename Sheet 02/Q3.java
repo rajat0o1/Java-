1c8efe9313423fb464e7109b59f4294e52fe521a
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-09-03 06:04:52 +0000

    Configuration Done

--- Sheet 02/Q3.java
+++ Sheet 02/Q3.java
@@ -7,12 +7,14 @@ class TeenAger{
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter Age as an Input: ");
         int age=sc.nextInt();
+        // 
         if ((age>12)&&  (age<20))
         System.out.println("Teenager: Beware of my Tantrums");
         else if (age>20)
         System.out.println("Young");
-        else 
+        else    
         System.out.print("Kid");
+
 
     }
 }
