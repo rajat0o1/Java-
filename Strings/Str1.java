614bc079967d81d6b29fdc07008a0a276930236f
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-09-03 23:32:12 +0000

    Configuration Done

--- Strings/Str1.java
+++ Strings/Str1.java
@@ -1,6 +1,8 @@
 // * Write a java program to convert the string from upper case to lower case.
 import java.util.Scanner;
-class StringBasics{
+import java.util.*;
+// import java.util.String;
+class StrBasic{
     public static void main(String[] args){
         String str="Hey! Welcome to My String Practice Repository...";
         System.out.println(str);
