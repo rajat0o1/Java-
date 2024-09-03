ec15fdeef7d60888ade294181a8883448dc10536
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-09-03 05:47:08 +0000

    Configuration Done

---
+++ Sheet 02/Que2.java
@@ -0,0 +1,30 @@
+// Take 3 numbers as input from user, print the largest number on the screen
+import java.util.Scanner;
+class LargestOfThree{
+    public static void main(String[] args){
+
+    Scanner sc= new Scanner(System.in); 
+    System.out.println("Enter Three Numbers: ");
+    int one = sc.nextInt();
+    int two = sc.nextInt();
+    int three = sc.nextInt();
+
+    if ((one > two) && (one > three))
+    {
+        System.out.println(one);
+    }
+    else if((two>one) && (two> three))
+    {
+        System.out.println(two);
+
+    }
+    else {
+        System.out.println(three);
+    }
+    }
+
+
+
+
+
+}
