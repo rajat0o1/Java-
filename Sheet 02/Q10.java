d61f764886966cef4c24e3b44f55c7a7c7d6df0f
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-09-07 19:40:37 +0000

    Configuration Done

---
+++ Sheet 02/Q10.java
@@ -0,0 +1,11 @@
+// 10 Write a program to check whether a number is divisible by 5 and 11 or not.
+import java.util.Scanner;
+class CheckDivisiblity{
+    public static void main(String[] args){
+        Scanner sc= new Scanner (System.in);
+        int num = sc.nextInt();
+        scanner.close();
+        
+
+    }
+}
