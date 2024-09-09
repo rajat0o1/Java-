9d728c1671212c627b86ae01a7510af9b8836c4b
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-09-09 07:34:16 +0000

    Configuration Done

---
+++ Sheet 3/Q4.java
@@ -0,0 +1,13 @@
+// Write a program to print all even numbers between 1 to 100.
+import java.util.Scanner;
+class PrintEven{
+    public static void main(String[] args){
+
+        for (int i=1;i<100;i++)
+    {
+        if (i%2==0)
+        System.out.println(i+" ");
+
+    }
+    }
+}
