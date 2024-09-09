903f7f2940c3175c8e7766db94a17e5b9b7454b5
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-09-09 12:48:30 +0000

    Configuration Done

---
+++ Sheet 3/Q10.java
@@ -0,0 +1,16 @@
+// Write a program to count number of digits in any number.
+import java.util.Scanner;
+class NumOfDigit{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int i=0,j,k;
+        System.out.print("Enter Number: ");
+        j= sc.nextInt();
+        for( i=0;i<=j/10;i++){
+            
+        }
+            System.out.print(i);
+
+
+    }
+}
