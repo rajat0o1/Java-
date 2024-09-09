afb1ce4016269b4148580eb3718ea1ccaffb76ef
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-09-09 06:10:12 +0000

    Configuration Done

---
+++ Sheet 02/Q21.java
@@ -0,0 +1,12 @@
+// Write a program to input basic salary of an employee and calculate its Gross salary
+// according to following:
+// Basic Salary <= 10000 : HRA = 20%, DA = 80%
+// Basic Salary <= 20000 : HRA = 25%, DA = 90%
+// Basic Salary > 20000 : HRA = 30%, DA = 95%
+
+import java.util.Scanner;
+class GrossSallery{
+    public static void main(String[] args){
+
+    }
+}
