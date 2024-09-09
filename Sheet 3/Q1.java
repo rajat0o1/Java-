bb371e38109b5a817fd5360786d6c5a1417a2787
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-09-09 06:15:31 +0000

    Configuration Done

---
+++ Sheet 3/Q1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class NaturalNumbers{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter length for Natural Number: ");
+        int num=sc.nextInt();
+        for (int i=1;i<=2000;i++){
+            if (i<=num)
+            System.out.println(i);
+        }
+
+    }
+}
