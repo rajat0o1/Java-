a2f7de8d2a600b97748c4d1231222332835ed97d
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-09-09 12:49:19 +0000

    Configuration Done

--- Sheet 3/Q1.java
+++ Sheet 3/Q1.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+// Write a program to print all natural numbers from 1 to n.import java.util.Scanner;
 class NaturalNumbers{
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
