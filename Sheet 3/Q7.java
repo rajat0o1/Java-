d7dcdd0b3a578069c3f9f03e95f2cca1ec566e67
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-09-09 10:09:45 +0000

    Configuration Done

---
+++ Sheet 3/Q7.java
@@ -0,0 +1,18 @@
+// Write a program to find sum of all even numbers between 1 to n.
+import java.util.Scanner;
+class SumEven{
+    public static void main(String[] rajat){
+        Scanner sc= new Scanner(System.in);
+        int j=0,k,x;
+        System.out.print("Enter Number: ");
+        k= sc.nextInt();
+        for (int i=1;i<=k*2;i++){
+            if (i%2==0){
+                j=i+j;
+            }
+        }
+        System.out.println("Sum of "+k+" even Numbers is: "+ j);
+
+        
+    }
+}
