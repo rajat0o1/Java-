088010c985b5ffc5840dfa124abc7307a363741f
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-09-12 18:19:40 +0000

    Configuration Done

---
+++ Sheet 3/Q11.java
@@ -0,0 +1,20 @@
+// 11 Write a program to find first and last digit of any number.                   6482 6, 2,
+
+
+import java.util.Scanner;
+class FirstLast{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter Number: ");
+        int x= sc.nextInt();
+        while(x>0){
+            x
+
+
+
+        }
+
+
+
+    }
+}
