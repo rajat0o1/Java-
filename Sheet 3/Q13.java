9c4aed531a2ac1d58c2ef92d429841714f24af0f
Rajat Verma <120889962+rajat0o1@users.noreply.github.com>
2024-09-13 05:02:55 +0000

    Coniguration Done

---
+++ Sheet 3/Q13.java
@@ -0,0 +1,28 @@
+// 13 Write a program to swap first and last digits of any number. 6482 2486
+
+
+import java.util.Scanner;
+class SwapFirstLast{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number: ");
+        int num= sc.nextInt();
+        if(num<0){
+          num=-num;
+        }
+        int first=num;
+        int last= num;
+
+
+
+
+      while(first>10){
+        first=first/10;
+
+      }
+        last=num%10;
+
+
+            
+    }
+}
